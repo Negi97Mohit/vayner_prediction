8faf5572a12ffade834840578d9fc70067a4d0e3
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2023-07-09 14:19:05 -0400

    environment.yml

--- Home.py
+++ Home.py
@@ -8,19 +8,19 @@ import base64
 st.set_page_config(layout="wide")
 
 def main():
-    with open('5183000.jpg', "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
+    # with open('5183000.jpg', "rb") as image_file:
+    #     encoded_string = base64.b64encode(image_file.read())
+    # st.markdown(
+    # f"""
+    # <style>
+    # .stApp {{
+    #     background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+    #     background-size: cover
+    # }}
+    # </style>
+    # """,
+    # unsafe_allow_html=True
+    # )
 
     st.title('Facebook Marketing Analysis')
     facebook=pd.read_excel('Analyst_Candidate_Dataset_.xlsx',sheet_name='Facebook Data')    
@@ -49,7 +49,7 @@ def main():
         </style>
         """, unsafe_allow_html=True)
         st.markdown('<p class="big-font">Insight!</p>', unsafe_allow_html=True)
-        st.write('It is clear from the graph that a certain spike in video views was experfienced b/w feb 11 and 14 lets explore why??')
+        st.write('It is clear from the graph that a certain spike in video views was experfienced b/w feb 11 and 14, lets explore why??')
     
 def why():
     facebook=pd.read_excel('Analyst_Candidate_Dataset_.xlsx',sheet_name='Question 4- Combining Data')
@@ -59,12 +59,27 @@ def why():
         fig=px.bar(groups_gender[['Total Revenue','Media Spend']],height=400,width=600)
         st.plotly_chart(fig)    
     with cols2:
-        groups_gender=facebook.groupby(['Date','Campaign','Gender']).sum()
-        groups_gender.reset_index()
-        fig=px.bar(groups_gender[['Date','Total Revenue','Media Spend']],height=400,width=600)
+        # groups_gender=facebook.groupby(['Date','Campaign','Gender']).sum()
+        # groups_gender.reset_index()
+        # fig=px.bar(groups_gender[['Date','Total Revenue','Media Spend']],height=400,width=600)
+        # st.plotly_chart(fig)
+        groups_gender=facebook.groupby(['Campaign']).sum()
+        groups_gender.reset_index(inplace=True)
+
+
+        fig=px.bar(groups_gender[['Campaign','Total Revenue','Media Spend']],x='Campaign',y=['Total Revenue','Media Spend'],barmode='group',height=400,width=600) #,y='Media Spend',barmode='group',height=400,width=600)
         st.plotly_chart(fig)
-    
-       
+
+    st.markdown("""
+        <style>
+        .big-font {
+            font-size:50px !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Insight!</p>', unsafe_allow_html=True)
+    st.write('It is clear from the above two graphs that Evergreen was the most profitable campaign we had. Free shipping campaign had low media spend but a good revenue was attained.')
+   
 if __name__=='__main__':
     main()
     why()
